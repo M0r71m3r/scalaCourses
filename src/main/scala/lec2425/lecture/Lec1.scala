6d010c9554bb361d004c2fa92efb0f74d77ff274
Pavel Ulanov <ul4n0vp4v3l@yandex.ru>
2024-10-15 17:34:36 +0400

    prepare

--- src/main/scala/lec2425/lecture/Lec1.scala
+++ src/main/scala/lec2425/lecture/Lec1.scala
@@ -5,3 +5,13 @@ object HelloWorld {}
 object HelloWorldWithApp extends App {}
 
 object TypesAndAllALL extends App {}
+
+object StringSmthng extends App {}
+
+object TupleSmthng extends App {}
+
+object DefFun extends App {}
+
+object DefaultDefParam extends App {}
+
+object ValFun extends App {}
